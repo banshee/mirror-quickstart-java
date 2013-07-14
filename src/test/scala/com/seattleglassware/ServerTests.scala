49aa2c35f7ecf8575a3208813fbb334127813026
James Moore <james@Jamess-MacBook-Pro.local>
2013-07-14 15:35:55 -0700

    removed obsolete test

--- src/test/scala/com/seattleglassware/ServerTests.scala
+++ src/test/scala/com/seattleglassware/ServerTests.scala
@@ -83,8 +83,6 @@ class AuthUtilTests extends FunSuite with ShouldMatchers with MockitoSugar {
   test("urlPathMatch should match") {
     implicit val bindingmodule = TestBindings.configuration
     val r = new TestHttpRequestWrapper("http://example.com/oauth2callback")
-    val result = (new AuthFilterSupport {}).urlPathMatches { case "oauth2callback" :: Nil => true }(r)
-    result should be(true)
   }
 
   test("AuthFilter should redirect to https if the hostname is appspot") {
