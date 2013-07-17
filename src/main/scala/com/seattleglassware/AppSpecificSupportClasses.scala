312db862e41554d5ac69f2ac513b1b3330b088b3
James Moore <james@restphone.com>
2013-07-17 08:06:47 -0700

    clean up imports

--- src/main/scala/com/seattleglassware/AppSpecificSupportClasses.scala
+++ src/main/scala/com/seattleglassware/AppSpecificSupportClasses.scala
@@ -3,57 +3,41 @@ package com.seattleglassware
 import java.io.FileInputStream
 import java.io.InputStream
 import java.util.Properties
+
 import scala.collection.JavaConversions.seqAsJavaList
-import scala.collection.JavaConverters.asScalaBufferConverter
 import scala.collection.JavaConverters.collectionAsScalaIterableConverter
+import scala.language.higherKinds
 import scala.util.control.Exception.ultimately
-import scala.PartialFunction._
+
 import com.escalatesoft.subcut.inject.BindingModule
 import com.escalatesoft.subcut.inject.Injectable
 import com.escalatesoft.subcut.inject.bindingIdToString
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow
-import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.auth.oauth2.CredentialStore
 import com.google.api.client.extensions.appengine.http.UrlFetchTransport
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
 import com.google.api.client.http.GenericUrl
 import com.google.api.client.json.jackson.JacksonFactory
 import com.google.api.client.util.ByteStreams
-import com.google.api.services.mirror.Mirror
-import com.seattleglassware.BindingIdentifiers._
-import JavaInterop.asInstanceOfNotNull
+import com.seattleglassware.BindingIdentifiers.ApplicationName
+import com.seattleglassware.BindingIdentifiers.GlassScope
+import com.seattleglassware.BindingIdentifiers.OAuthPropertiesFileLocation
+import com.seattleglassware.EitherTWithState._
+import com.seattleglassware.GlasswareTypes._
+import com.seattleglassware.Misc.GenericUrlWithNewScheme
+
+import HttpRequestWrapper.HttpServletRequestWrapper
 import JavaInterop.safelyCall
-import javax.servlet.Filter
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
-import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import scalaz.Lens
-import scalaz.Monoid
-import scalaz.EitherT
-import scalaz.Bind
-import scalaz.Applicative
-import scalaz.MonadTrans
+import scalaz._
+import scalaz.{-\/ => -\/}
+import scalaz.{\/ => \/}
+import scalaz.{\/- => \/-}
 import scalaz.Scalaz._
-import scalaz.State
-import scalaz.\/
-import scalaz.-\/
-import scalaz.\/-
-import com.seattleglassware.EitherTWithState._
-import GlasswareTypes._
-import com.google.api.client.auth.oauth2.TokenResponse
-import com.google.api.client.auth.oauth2.Credential.AccessMethod
-import com.google.api.client.http.HttpExecuteInterceptor
-import com.google.api.client.http.HttpRequestInitializer
-import com.google.api.client.util.Clock
-import HttpRequestWrapper._
-import com.seattleglassware.Misc._
-import com.seattleglassware.GlasswareTypes._
-import com.google.api.client.auth.oauth2.CredentialStoreRefreshListener
-import com.google.api.services.mirror.model.Contact
-import com.google.api.services.mirror.model.Subscription
 
 object GlasswareTypes {
   val stateTypes = StateGenerator[GlasswareState, EarlyReturn]
