9fa05ec5b8fcbcf6ce83a0f7912a19bda7f11c6c
James Moore <james@restphone.com>
2013-07-17 08:04:32 -0700

    clean up imports

--- src/main/scala/com/seattleglassware/SupportClasses.scala
+++ src/main/scala/com/seattleglassware/SupportClasses.scala
@@ -1,30 +1,11 @@
 package com.seattleglassware
 
-import java.io.FileInputStream
-import java.io.InputStream
-import java.util.Properties
-
-import scala.collection.JavaConversions.seqAsJavaList
 import scala.collection.JavaConverters.asScalaBufferConverter
-import scala.collection.JavaConverters.collectionAsScalaIterableConverter
-import scala.util.control.Exception.ultimately
-import scala.PartialFunction._
 
 import com.escalatesoft.subcut.inject.BindingModule
-import com.escalatesoft.subcut.inject.Injectable
-import com.escalatesoft.subcut.inject.bindingIdToString
-import com.google.api.client.auth.oauth2.AuthorizationCodeFlow
-import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.auth.oauth2.CredentialStore
-import com.google.api.client.extensions.appengine.http.UrlFetchTransport
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
 import com.google.api.client.http.GenericUrl
-import com.google.api.client.json.jackson.JacksonFactory
-import com.google.api.client.util.ByteStreams
-import com.google.api.services.mirror.Mirror
-import com.seattleglassware.BindingIdentifiers.ApplicationName
-import com.seattleglassware.BindingIdentifiers.OAuthPropertiesFileLocation
 
+import GlasswareTypes._
 import JavaInterop.asInstanceOfNotNull
 import JavaInterop.safelyCall
 import javax.servlet.Filter
@@ -34,17 +15,8 @@ import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import scalaz.Lens
-import scalaz.Monoid
-import scalaz.EitherT
-import scalaz.Applicative
-import scalaz.MonadTrans
 import scalaz.Scalaz._
-import scalaz.State
 import scalaz.\/
-
-import com.seattleglassware.EitherTWithState._
-import GlasswareTypes._
 
 object Misc {
   type =?>[A, B] = PartialFunction[A, B]
